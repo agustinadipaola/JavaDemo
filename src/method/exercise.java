4eeb7f89cc80e72a37aa15bf26cd0ee6321b7ca7
unknown <agustinadipaolauk@gmail.com>
2024-01-12 14:48:50 +0000

    update exercise

---
+++ src/method/exercise.java
@@ -0,0 +1,40 @@
+package method;
+
+public class exercise {
+	public static void main(String[] args)
+//	{System.out.println(add (10, 5));
+//	}
+//	 {
+//		System.out.println(minus (10, 5));
+//	}
+//	
+//	 {
+//		System.out.println(multiply (10, 5));
+//	}
+//	
+	 {
+		System.out.println(divide (10, 5));
+	}
+	
+	
+	
+//	public static int add (int a, int b) {
+//		return (a+b);
+//	
+//	}
+	
+//	public static int minus (int a, int b) {
+//		return (a-b);
+//	
+//}
+//	public static int multiply (int a, int b) {
+//		return (a*b);
+//	
+//	}
+
+	public static int divide (int a, int b) {
+		return (a/b);
+	
+	}
+	
+}
