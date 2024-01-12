b16d93b1385820a787f43309e333433ae2cc7fd0
unknown <agustinadipaolauk@gmail.com>
2024-01-12 12:16:31 +0000

    update exercise

--- src/Exercise_12_01_24/Exercise.java
+++ src/Exercise_12_01_24/Exercise.java
@@ -1,5 +1,34 @@
 package Exercise_12_01_24;
 
 public class Exercise {
+	public static void main (String []args) {
+		
+		int i= 2000;
+		char c= 'a';
+		boolean sleepy =true;
+		byte b= 3;
+		long l= 999999999L;
+		short s=2;
+		double d= 11.11;
+		float f= 1234.56F;
+		
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(sleepy);
+		System.out.println(b);
+		System.out.println(l);
+		System.out.println(s);
+		System.out.println(d);
+		System.out.println(f);
+		
+		
+		String firstpartName = "Agus";
+		String secondpartName = "Tina";
+		
+		System.out.println(firstpartName.concat(secondpartName));
+		
+		
+		
+	}
 
 }
